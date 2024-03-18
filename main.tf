fd3639f39e15f086a97105cfb26616b62cde68b0
DeshnaBera <beradeshna@gmail.com>
2024-03-18 12:31:19 +0530

    added ebs volume

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ resource "aws_instance" "example" {
     Name = var.instance_name
   }
 }
+
+# Create an EBS volume
+resource "aws_ebs_volume" "example_volume" {
+  availability_zone = var.availability_zone # Must be in the same AZ as the instance
+  size              = var.size            # Size in GB
+
+  tags = {
+    Name = var.volume_name,
+    Snapshot = var.volume_snapshot
+  }
+}
+
+# Attach the EBS volume to the EC2 instance
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = var.device_name
+  volume_id   = aws_ebs_volume.example_volume.id
+  instance_id = aws_instance.example.id
+}
--- variables.tf
+++ variables.tf
@@ -10,3 +10,23 @@ variable "instance_type" {
 variable "instance_name" {
   description = "The name tag for the EC2 instance"
 }
+
+variable "availability_zone" {
+  description = "The availability zone of the EBS volume"
+}
+
+variable "size" {
+  description = "The size of EBS volume in GB"
+}
+
+variable "volume_name" {
+  description = "The name tag for the EBS Volume"
+}
+
+variable "volume_snapshot" {
+  description = "The snapshot tag for the EBS Volume"
+}
+
+variable "device_name" {
+  description = "The device name for EBS volume"
+}
